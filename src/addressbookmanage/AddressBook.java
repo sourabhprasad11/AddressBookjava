2144eebafb2b97f8ec061caf925e690c3bb24b5a
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-24 10:45:30 +0530

    Switch case included in the main class(AddressBook)

--- src/addressbookmanage/AddressBook.java
+++ src/addressbookmanage/AddressBook.java
@@ -8,6 +8,53 @@ public class AddressBook {
 
 	public static void main(String[] args) {
 		
+		AddressbookmanageImp abmi=new AddressbookmanageImp();
+		System.out.println("1. New AddressBook");
+		System.out.println("2. Open AddressBook");
+		System.out.println("3. Save AddressBook");
+		System.out.println("4. SaveAs AddressBook");
+		System.out.println("5. Close AddressBook");
+		System.out.println("0. Quit");
+		
+		Scanner ip=new Scanner(System.in);
+		System.out.println("Enter the number from above: ");
+		int ans=ip.nextInt();
+		
+		do {
+		switch(ans) {
+			case 1: 
+					abmi.newAddressBook();
+					//System.out.println("working");
+					break;
+			case 2: 
+					abmi.openAddressBook();
+					//System.out.println("working");
+				break;
+			case 3: 
+					abmi.saveAddressBook();
+					//System.out.println("working");
+				break;
+			case 4: 
+					abmi.saveasAddressBook();
+					//System.out.println("working");
+				break;
+			case 5: 
+					abmi.closeAddressBook();
+					//System.out.println("working");
+				break;
+			case 6: 
+					abmi.quit();
+					//System.out.println("working");
+				break;
+			default:
+					System.out.println("You have entered the wrong number...Terminating the application");
+					ans=0;
+				break;
+			}
+		}while(ans!=0);
+		
+		ip.close();
+
 		
 	}
 
