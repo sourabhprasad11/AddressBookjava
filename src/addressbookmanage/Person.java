78e8c952dc134aefd9080ed41ea78bde960e7a76
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-24 10:50:17 +0530

    class instances and contructor of the Person class passed

--- src/addressbookmanage/Person.java
+++ src/addressbookmanage/Person.java
@@ -2,7 +2,26 @@ package addressbookmanage;
 
 
 public class Person {
+	  public final String firstname;
+	  public final String lastname;
+	  public final long phone;
+	  public final int zipcode;
+	  //public final String address;
+	  public final String city;
+	  public final String state;
+
+
+public Person(String firstname,String lastname,long phone, int zipcode,String city ,String state){
 	
+	  //super();
+	  this.firstname=firstname;
+	  this.lastname=lastname;
+	  this.phone=phone;
+	  this.zipcode=zipcode;
+	  this.city=city; 
+	  this.state=state;
+
+}
 	
 
 }
